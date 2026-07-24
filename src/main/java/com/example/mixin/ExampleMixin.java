a5136817af1885742fe7b301a6573cbd710c71c7
『𝕀𝕞𝕡𝕣𝕖𝕧𝕦』 <simon.durlea@gmail.com>
2026-07-24 22:12:19 +0200

    Update ExampleMixin to modify FPS in MinecraftClient
    Changed the mixin target from MinecraftServer to MinecraftClient and added a method to modify the FPS return value.

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,18 @@
 package com.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+@Mixin(MinecraftClient.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+
+    @Inject(method = "getCurrentFps", at = @At("RETURN"), cancellable = true)
+    private static void modifyFps(CallbackInfoReturnable<Integer> cir) {
+        int realFps = cir.getReturnValue();
+        // Additionne 2500 aux vrais FPS
+        cir.setReturnValue(realFps + 2500);
+    }
 }
