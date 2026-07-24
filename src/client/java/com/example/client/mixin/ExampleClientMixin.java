fcda9d597dde7e7be229d435cc6e033ef6a85913
『𝕀𝕞𝕡𝕣𝕖𝕧𝕦』 <simon.durlea@gmail.com>
2026-07-24 22:24:04 +0200

    Rename class and swap method injections for FPS

--- src/client/java/com/example/client/mixin/ExampleClientMixin.java
+++ src/client/java/com/example/client/mixin/ExampleClientMixin.java
@@ -7,15 +7,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftClient.class)
-public class ExampleMixin {
-
-    @Inject(method = "getFps", at = @At("RETURN"), cancellable = true, require = 0)
-    private static void onGetFps(CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(cir.getReturnValue() + 2500);
-    }
+public class ExampleClientMixin {
 
     @Inject(method = "getCurrentFps", at = @At("RETURN"), cancellable = true, require = 0)
     private static void onGetCurrentFps(CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(cir.getReturnValue() + 2500);
+    }
+
+    @Inject(method = "getFps", at = @At("RETURN"), cancellable = true, require = 0)
+    private static void onGetFps(CallbackInfoReturnable<Integer> cir) {
         cir.setReturnValue(cir.getReturnValue() + 2500);
     }
 }
